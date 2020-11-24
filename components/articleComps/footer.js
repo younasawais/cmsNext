450cc4c7e2783a182e15c7fabadd90b5906ee69b
Younas Awais <younasawais@gmail.com>
2020-11-24 00:26:17 -0500

    homepage style done, data fetching works

--- components/articleComps/footer.js
+++ components/articleComps/footer.js
@@ -3,8 +3,8 @@ import style from '../../styles/Footer.module.css';
 
 export function Footer(){
     return(
-        <div style={{width: '100%', textAlign:'center' , height: 40, backgroundColor: '#2e2e2e', width:'100%'}}>
-           <p style={{color:'white'}} className="align-center display-7">
+        <div style={{width: '100%', textAlign:'center' , height: 50, backgroundColor: '#2e2e2e', width:'100%'}}>
+           <p style={{color:'white', paddingTop:7}} className="align-center display-7">
              © Copyright 2020 Qouh - All Rights Reserved
            </p>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,76 @@
 //import Link from 'next/link';
 import React, {Fragment} from 'react';
-import styles from '../components/home.module.css';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Menu from '../components/nav';
+import axios from 'axios';
 
-export default function Home(){
+// API load time: https://nuqf.com/getmenunamesandlinks
+Home.getInitialProps = async () => {
+    const {data} = await axios.post('https://nuqf.com/getmenunamesandlinks');
+    return {menuLinks : data.menuLinks, menus: data.menus}
+}
+
+export default function Home({menuLinks, menus}){
     return (
         <Fragment>
-        <section className="header5 cid-s9NcstqoeV mbr-fullscreen" id="header5-h" style={{background: 'linear-gradient(0deg, #f3e4cb, #00000091, #59e0ca)'}}>
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="mbr-white col-md-10">
-                        <h1 className="mbr-section-title align-center pb-3 mbr-fonts-style display-1">
-                            Qouh
-                        </h1>
-                        <p className="mbr-text align-center display-5 pb-3 mbr-fonts-style">
-                        “Wisdom is not a product of schooling but of the lifelong attempt to acquire it.” ― Albert Einstein
-                        </p>
-                        <div className="mbr-section-btn align-center">
-                            {/* {menus.map((menu, i)=>{return(
-                                <Link key={i} type="button" to={'/article/'+menuLinks[i].linkId} className="btn btn-md btn-white-outline display-4">{menu.name}</Link>
-                            )})} */}
-                            <Button type="button" href='/article' variant="outline-secondary" >Testing</Button>
-                            <Button type="button" href='/article' variant="outline-secondary" >Testing</Button>
-                            <Button type="button" href='/article' variant="outline-secondary" >Testing</Button>
-                        </div>
+        <section style={style.section}>
+                <div style={style.container}>
+                    <h1 style={style.h1}>
+                        QOUH
+                    </h1>
+                    <p style={style.p}>
+                    “Wisdom is not a product of schooling but of the lifelong attempt to acquire it.” ― Albert Einstein
+                    </p>
+                    <div style={style.buttons}>
+                        {menus.map((menu, i)=>{return(
+                            <Button key={i} style={style.btn} type="button" href={"/article/" + menuLinks[i].linkId} variant="outline-light" >{menu.name}</Button>
+                        )})}
                     </div>
                 </div>
-            </div>
         </section>
     </Fragment>
     );
+}
+
+const style = {
+    btn : {
+        borderRadius:'100px',
+        fontWeight: 500,
+        letterSpacing: '1px',
+        marginTop: '.4rem',
+        marginRight: '.8rem',
+        marginBottom: '.4rem',
+        marginLeft: '.8rem'
+    },
+    section : {
+        background: 'linear-gradient(0deg, #f3e4cb, #00000091, #59e0ca)',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        paddingTop: '3rem',
+        paddingBottom: '3rem'},
+    container : {
+        maxWidth: '980px',
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '5px 5px 5px 5px',
+        textAlign: 'center',
+        alignSelf: 'center',
+        justifyContent: 'center'},
+    h1 : {
+        alignSelf: 'center',
+        fontFamily: 'Rubik, sans-serif',
+        color: 'white',
+        fontSize: '4.25rem'},
+    p  : {
+        alignSelf: 'center',
+        fontFamily: 'Rubik, sans-serif',
+        fontSize: '1.5rem',
+        color: 'white',
+        textAlign: 'center'
+        },
+    buttons : {
+        alignSelf: 'center'
+        }
 }
