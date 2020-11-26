aaa8b2d82ee0deec2c1bb4ae387c3ec3de149d11
Younas Awais <younasawais@gmail.com>
2020-11-25 23:53:29 -0500

    updated image link

--- components/articleComps/articlePicture.js
+++ components/articleComps/articlePicture.js
@@ -3,7 +3,7 @@ import Image from 'react-bootstrap/Image';
 export function ArticlePicture({imageName1}){
     return(
         <section style={{display: 'flex', justifyContent: "space-around"}}>
-            <Image src={"https://qouh.com/images/" + imageName1} fluid />
+            <Image src={"https://militating.com/qouh.com/images/" + imageName1} fluid />
     </section>
     )
 }
